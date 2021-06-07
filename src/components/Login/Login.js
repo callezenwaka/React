617c8b2742012f355d9b24b89f572f7942bd7c64
callezenwaka <callisezenwaka@gmail.com>
2021-06-07 12:16:06 +0000

    Use the useReducer and useEffect hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer  } from 'react';
+import React, { useState, useEffect, useReducer  } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -38,21 +38,27 @@ const Login = (props) => {
     value: '',
     isValid: null,
   });
+  
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+  
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier);
+    };
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
 
-    setFormIsValid(
-      event.target.value.includes('@') && passwordState.isValid
-    );
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({type: 'USER_INPUT', val: event.target.value});
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && emailState.isValid
-    );
   };
 
   const validateEmailHandler = () => {
